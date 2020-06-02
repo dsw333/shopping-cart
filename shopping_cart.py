18cbb3d7bbbd5fc69f43ecef6a55bcb7490d81eb
dsw333 <65722424+dsw333@users.noreply.github.com>
2020-06-01 22:21:24 -0400

    Walkthrough complete
    Need to QA
    Format to USD and continue to finish per assignment

--- shopping_cart.py
+++ shopping_cart.py
@@ -39,29 +39,22 @@ def to_usd(my_price):
 
 print(products)
 
-
-# Info Capture/ Input
-
+total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ")
     if selected_id == "DONE":
         break
-    else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"])
+    else:    
+        selected_ids.append(selected_id)
 
+print(selected_ids)
 
-print(matching_product)
-print(type(matching_product))
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])
 
-
-
-
-
-
-
-#Info Display/ OUtput 
-
-
+print("TOTAL PRICE: " + str(total_price))
