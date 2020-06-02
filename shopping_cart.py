c4d7f20aa32cf0960ab63c1d9ae8d0bc98d7b6cf
dsw333 <65722424+dsw333@users.noreply.github.com>
2020-06-01 21:48:11 -0400

    DONE Keyword committed

--- shopping_cart.py
+++ shopping_cart.py
@@ -42,10 +42,15 @@ print(products)
 
 # Info Capture/ Input
 
-selected_id = input("Please input a product identifier: ")
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-matching_product = matching_products[0]
-print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"])
+
+while True:
+    selected_id = input("Please input a product identifier: ")
+    if selected_id == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"])
 
 
 print(matching_product)
